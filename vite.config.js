947dd2c3ad5a0b138df447f912730646bcf1ab31
20FTT1446 <20FTT1446@student.pb.edu.bn>
2022-04-16 10:54:29 +0800

    remove line 11 on gitignore

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-https://vitejs.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '20ftt1446.github.io',
+  // base: '20ftt1446.github.io',
   plugins: [vue()]
 })
